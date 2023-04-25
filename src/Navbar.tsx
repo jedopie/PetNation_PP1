9c21d33196f546da69a69522da2185a14f04e1e1
jaimz <s3855148@student.rmit.edu.au>
2023-04-25 14:52:03 +1000

    CSS file created and added to the navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,9 +1,12 @@
+import "./Navbar.css";
+
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1>PetNation</h1>
+      <h1>
+        <a href="/">PetNation</a>
+      </h1>
       <div>
-        <a href="/"> Home</a>
         <a href="/sign-up"> SignUp</a>
         <a href="/about"> About Us</a>
       </div>
