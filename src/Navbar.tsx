6f3e3f30977bd25fa50c4256016ffe0a497d9147
jaimz <s3855148@student.rmit.edu.au>
2023-04-25 13:32:55 +1000

    Navbar file created with foundation

---
+++ src/Navbar.tsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h1>PetNation</h1>
+      <div>
+        <a href="/"> Home</a>
+        <a href="/sign-up"> SignUp</a>
+        <a href="/about"> About Us</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
