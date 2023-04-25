6793da07af05434de2970e68063e8ff7d5d2f772
jaimz <s3855148@student.rmit.edu.au>
2023-04-25 13:34:15 +1000

    Navbar added to app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
         <h1>Hello World</h1>
       </div>
